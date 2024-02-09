3199c9e6f4647a430422c4f812305ee22753da04
arthurkehrwald <50906979+arthurkehrwald@users.noreply.github.com>
2024-02-09 16:47:25 +0100

    Use pygame to display stream instead of opencv

--- display.py
+++ display.py
@@ -1,11 +1,37 @@
-import cv2
+import pygame
 from create_camera import create_camera
+import cv2
+import numpy as np
+from analyze import get_resolution
+
 
 if __name__ == "__main__":
+
+    pygame.init()
     with create_camera() as cam:
-        while True:
+        # Fit window to camera resolution
+        success, frame = cam.read_frame()
+        if success:
+            window_size = get_resolution(frame)
+        else:
+            window_size = (1280, 720)
+        screen = pygame.display.set_mode(window_size)
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
             success, frame = cam.read_frame()
             if success:
-                cv2.imshow("Frame", frame)
-            if cv2.waitKey(1) == ord('q'):
-                break
+                screen.fill("black")
+                # Adjust to pygame coordinate system and color format
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                frame = np.fliplr(frame)
+                frame = np.rot90(frame)
+                frame = pygame.surfarray.make_surface(frame)
+                # Blit the frame to the screen
+                screen.blit(frame, (0, 0))
+                pygame.display.flip()
+
+        pygame.quit()
